6ee45245640b89a0fbeba2d2805fbaa8477f59b3
Yu-zh <yu.zhang.yz862@yale.edu>
2020-11-19 12:25:23 -0500

    determ proof

--- examples/CompCertSem.v
+++ examples/CompCertSem.v
@@ -85,16 +85,78 @@ Section LTS.
 
 End LTS.
 
+Require Import Coqlib.
+Ltac determ_solve' :=
+  auto ||
+       match goal with
+       | [ |- False -> _ ] => inversion 1
+       | [ |- _ = _ -> _ ] => intros <-
+       | [ |- _ = _ /\ _ = _ -> _ ] => intros [<- <-]
+       | [ |- _ = _ /\ _ = _ /\ _ = _ -> _ ] => intros [<- [<- <-]]
+       | [ |- _ -> _ ] => intros
+       end.
+Ltac determ_solve determ :=
+  match goal with 
+  | [ P : _ , Q : _ |- _ ] =>
+    exploit determ;
+    [ exact P | exact Q | determ_solve' ]
+  | _ => fail
+  end.
+
 Section SEMANTICS.
   Context {liA liB} (L : semantics liA liB) (HL : determinate L).
 
+  
+  Lemma trace_determ se s es es' r r' :
+    list_coh liA es es' ->
+    lts_trace (L se) s es r ->
+    lts_trace (L se) s es' r' ->
+    es = es' /\ r = r'.
+  Proof.
+    intros coh h h'.
+    revert es' coh h'.
+    induction h; intros es' coh lts; inversion lts; subst.
+    + determ_solve (sd_final_determ (HL se)).
+    + determ_solve (sd_final_nostep (HL se)).
+    + determ_solve (sd_final_noext (HL se)).
+    + determ_solve (sd_final_nostep (HL se)).
+    + determ_solve (sd_determ_2 (HL se)).
+      eapply IHh. apply coh. apply H1.
+    + determ_solve (sd_at_external_nostep (HL se)).
+    + determ_solve (sd_final_noext (HL se)).
+    + determ_solve (sd_at_external_nostep (HL se)).
+    + specialize (IHh t0).
+      determ_solve (sd_at_external_determ (HL se)).
+      inversion coh as [ | | ? ? ? ? cohx cohxs]; subst.
+      destruct cohx as [cohq cohr].
+      exploit cohr. auto. intros <-. 
+      determ_solve (sd_after_external_determ (HL se)).
+      split; f_equal; apply IHh; try apply cohxs; auto.
+  Qed.
+  
   Program Definition compcerto_lmap se : !liA --o liB :=
     {|
       has '(t, u) := lts_lmaps (L se) t u;
     |}.
   Next Obligation.
-    (* ... *)
-  Admitted.
+    intros se [eas [qb rb]] [eas' [qb' rb']] lmap lmap' coheas.
+    split.
+    - split; auto.
+      intros <-.
+      inversion lmap as [? ? ? ? valid_q init_q transition_q]. subst.
+      inversion lmap' as [? ? ? ? valid_q' init_q' transition_q']. subst.
+      determ_solve (sd_initial_determ (HL se)).
+      exploit trace_determ.
+      exact coheas. exact transition_q. exact transition_q'.
+      intuition.
+    - intros h. destruct h.
+      inversion lmap as [? ? ? ? valid_q init_q transition_q]. subst.
+      inversion lmap' as [? ? ? ? valid_q' init_q' transition_q']. subst.
+      determ_solve (sd_initial_determ (HL se)).
+      exploit trace_determ. 
+      exact coheas. exact transition_q. exact transition_q'.
+      intuition.
+  Qed.
 
 End SEMANTICS.
 
@@ -107,10 +169,219 @@ Require Clight.
 
 (** We will need to prove this or the result may not be a linear map. *)
 
+  (* H0 : Clight.eval_expr (globalenv ((Clight.semantics1 p) se)) e le m a2 v2 *)
+Section EXPR_DETERM.
+  Variable ge: Clight.genv.
+  Variable e: Clight.env.
+  Variable le: Clight.temp_env.
+  Variable m: Memory.Mem.mem.
+
+  Lemma deref_loc_determ t mem loc ofs v1 v2:
+    Clight.deref_loc t mem loc ofs v1 ->
+    Clight.deref_loc t mem loc ofs v2 ->
+    v1 = v2.
+  Proof.
+    induction 1; inversion 1; subst; congruence.
+  Qed.
+  Ltac find_specialize :=
+    match goal with
+    | [ H : forall x, ?P x -> _, X : _, H1 : ?P ?X |- _ ] => specialize (H _ H1)
+    | [ H : forall x y, ?P x y -> _, X : _, Y : _,  H1 : ?P ?X ?Y |- _ ] => specialize (H _ _ H1)
+    | _ => idtac
+    end.
+  Ltac expr_determ_solve :=
+    repeat find_specialize; try split; f_equal; congruence || easy.
+  Lemma expr_determ:
+    (forall a v1,
+        Clight.eval_expr ge e le m a v1 ->
+        forall v2, 
+          Clight.eval_expr ge e le m a v2 ->
+          v1 = v2)
+    /\
+    (forall a b1 ofs1,
+        Clight.eval_lvalue ge e le m a b1 ofs1 ->
+        forall b2 ofs2,
+          Clight.eval_lvalue ge e le m a b2 ofs2 ->
+          b1 = b2 /\ ofs1 = ofs2).
+  Proof.
+    apply Clight.eval_expr_lvalue_ind.
+    - inversion 1; expr_determ_solve.
+    - inversion 1; expr_determ_solve.
+    - inversion 1; expr_determ_solve.
+    - inversion 1; expr_determ_solve.
+    - inversion 2; expr_determ_solve.
+    - intros. inversion H1; expr_determ_solve.
+    - intros. inversion H2; expr_determ_solve.
+    - intros. inversion H4; expr_determ_solve.
+    - intros. inversion H2; expr_determ_solve.
+    - inversion 1; expr_determ_solve.
+    - inversion 1; expr_determ_solve. 
+    - intros. inversion H2; subst; try easy.
+      exploit H0. exact H3.
+      intros [<- <-].
+      determ_solve deref_loc_determ.
+    - inversion 2; expr_determ_solve.
+    - inversion 3; expr_determ_solve.
+    - inversion 3; expr_determ_solve.
+    - intros. inversion H4; expr_determ_solve.
+    - intros. inversion H3; expr_determ_solve.
+  Qed.
+  Lemma eval_expr_determ:
+    forall a v1,
+      Clight.eval_expr ge e le m a v1 ->
+        forall v2, 
+          Clight.eval_expr ge e le m a v2 ->
+          v1 = v2.
+  Proof.
+    intros. eapply expr_determ; eauto.
+  Qed.
+  Lemma eval_lvalue_determ:
+    forall a b1 ofs1,
+      Clight.eval_lvalue ge e le m a b1 ofs1 ->
+      forall b2 ofs2,
+        Clight.eval_lvalue ge e le m a b2 ofs2 ->
+        b1 = b2 /\ ofs1 = ofs2.
+  Proof.
+    intros. eapply expr_determ; eauto.
+  Qed.
+  Lemma eval_exprlist_determ es ty vs1 vs2:
+    Clight.eval_exprlist ge e le m es ty vs1 ->
+    Clight.eval_exprlist ge e le m es ty vs2 ->
+    vs1 = vs2.
+  Proof.
+    intros eval1. revert vs2.
+    induction eval1.
+    - inversion 1. auto.
+    - intros vs2 eval2.
+      inversion eval2; subst.
+      determ_solve eval_expr_determ.
+      exploit IHeval1.
+      exact H8. congruence.
+  Qed.
+
+End EXPR_DETERM.
+
+Lemma assign_loc_determ ge t m loc ofs v m1 m2:
+    Clight.assign_loc ge t m loc ofs v m1 ->
+    Clight.assign_loc ge t m loc ofs v m2 ->
+    m1 = m2.
+Proof.
+  inversion 1; inversion 1; congruence.
+Qed.
+Lemma alloc_variables_determ ge e m vars e1 e2 m1 m2:
+    Clight.alloc_variables ge e m vars e1 m1 ->
+    Clight.alloc_variables ge e m vars e2 m2 ->
+    e1 = e2 /\ m1 = m2.
+Proof.
+  intros alloc1. revert e2 m2.
+  induction alloc1.
+  - inversion 1. auto.
+  - inversion 1; subst.
+    rewrite H in H8. injection H8. intros <- <-.
+    exploit IHalloc1. exact H9. auto.
+Qed.
+Lemma bind_parameters_determ ge e m params vargs m1 m2:
+  Clight.bind_parameters ge e m params vargs m1 ->
+  Clight.bind_parameters ge e m params vargs m2 ->
+  m1 = m2.
+Proof.
+  intros bind1. revert m2.
+  induction bind1.
+  - inversion 1. auto.
+  - inversion 1; subst.
+    assert (b = b0) by congruence. subst.
+    determ_solve assign_loc_determ.
+    exploit IHbind1. exact H11. auto.
+Qed.
+Lemma func_entry1_determ ge f vargs m e1 le1 m1 e2 le2 m2:
+  Clight.function_entry1 ge f vargs m e1 le1 m1 ->
+  Clight.function_entry1 ge f vargs m e2 le2 m2 ->
+  e1 = e2 /\ le1 = le2 /\ m1 = m2.
+Proof.
+  inversion 1. inversion 1.
+  determ_solve alloc_variables_determ.
+  firstorder. congruence.
+  determ_solve bind_parameters_determ.
+Qed.
+
+Ltac false_solve :=
+  match goal with
+  | [ H : _ \/ _ |- _ ] => inversion H; easy
+  | _ => idtac
+  end.
+
+Hint Constructors match_traces.
+Ltac autoc := auto || congruence || easy.
+Lemma step_determ p se s t1 s1 t2 s2:
+  Step ((Clight.semantics1 p) se) s t1 s1 ->
+  Step ((Clight.semantics1 p) se) s t2 s2 -> 
+  match_traces se t1 t2 /\ (t1 = t2 -> s1 = s2).
+Proof.
+  intros step1 step2.
+  inversion step1; subst;
+    inversion step2; subst; false_solve;
+      try (split; autoc).
+  + determ_solve eval_expr_determ.
+    determ_solve eval_lvalue_determ.
+    assert (v = v1) as <- by congruence.
+    determ_solve assign_loc_determ.
+    split; autoc.
+  + determ_solve eval_expr_determ.
+    split; auto.
+  + determ_solve eval_expr_determ.
+    assert (tyargs0 = tyargs) by congruence. subst.
+    determ_solve eval_exprlist_determ.
+    split; auto.
+  + determ_solve eval_exprlist_determ.
+    determ_solve external_call_determ.
+    split. apply H1. 
+    intros. exploit (proj2 H1).
+    auto. intros [<- <-]. auto.
+  + determ_solve eval_expr_determ.
+    split. auto.
+    assert (b = b0) by congruence. subst; auto.
+  + split; try autoc.
+    determ_solve eval_expr_determ. autoc.
+  + determ_solve eval_expr_determ. split; autoc.
+  + assert (f = f0) by congruence. subst.
+    determ_solve func_entry1_determ.
+    split; autoc.
+  + assert (ef = ef0) by congruence. subst.
+    determ_solve external_call_determ.
+    split. apply H0.
+    intros. exploit (proj2 H0). auto.
+    intros [<- <-]. auto.
+Qed.
+
+Lemma clight_single_event p se:
+  single_events ((Clight.semantics1 p) se).
+Proof.
+  unfold single_events. intros.
+  inversion H; auto; eapply external_call_trace_length; eauto.
+Qed.
+Hint Unfold globalenv.
+Hint Unfold Clight.globalenv.
 Lemma clight_determinate p :
   determinate (Clight.semantics1 p).
 Proof.
-Admitted.
+  split.
+  - apply step_determ.
+  - apply clight_single_event.
+  - inversion 1; inversion 1; congruence.
+  - inversion 1.
+    replace (Clight.globalenv se p) with (globalenv ((Clight.semantics1 p) se)) in H0 by auto.
+    inversion 1; subst; rewrite H0 in FIND; subst f.
+    + easy.
+    + injection FIND. intros <- <- <- <-. 
+      easy.
+  - inversion 1; inversion 1; subst. f_equal.
+    assert (f = f0) by congruence.
+    subst f f0. congruence.
+  - inversion 1; inversion 1; subst. auto.
+  - inversion 1; inversion 1.
+  - inversion 1; inversion 1.
+  - inversion 1; inversion 1; subst. auto.
+Qed.
 
 Definition clight (p : Clight.program) se : !li_c --o li_c :=
   compcerto_lmap (Clight.semantics1 p) (clight_determinate p) se.
