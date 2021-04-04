c8d7ea317f4a3d137af428407582f29c8d4f129f
Yu-zh <yu.zhang.yz862@yale.edu>
2021-04-04 01:30:06 -0400

    layer comp fsim

--- compcertxo/CatComp.v
+++ compcertxo/CatComp.v
@@ -3,9 +3,133 @@ Require Import List.
 Require Import Coqlib.
 Require Import LanguageInterface Events Globalenvs SmallstepLinking Smallstep.
 
+(* Note that in a more general setting, the categorical composition takes two
+   transition systems A ↠ B and B ↠ C and returns A ↠ C. Moreover, we'd like
+   effect signature to replace the valid_query predicate to better handle the
+   domain. Right now, valid_query q = true means q will invoke transitions and
+   valid_query q = false means q will be treated as an external call during
+   transition.  The problem here is that the two kinds queries are not
+   mutually exclusive *)
 
-Section CAT_COMP.
-  Context {li} (L1 L2: semantics li li).
+Module CAT_COMP.
+  Section CAT_COMP.
+    Context {li} (L1 L2: semantics li li).
+
+    Section WITH_SE.
+      Context (se: Genv.symtbl).
+      Variant state :=
+      | st1 (s: Smallstep.state L1)
+      | st2 (s1: Smallstep.state L1) (s2: Smallstep.state L2).
+
+      Inductive step: state -> trace -> state -> Prop :=
+      | step1 s1 t s1':
+          Step (L1 se) s1 t s1' ->
+          step (st1 s1) t (st1 s1')
+      | step2 s1 s2 t s2':
+          Step (L2 se) s2 t s2' ->
+          step (st2 s1 s2) t (st2 s1 s2')
+      | step_push s1 q s2:
+          Smallstep.at_external (L1 se) s1 q ->
+          valid_query (L2 se) q = true ->
+          Smallstep.initial_state (L2 se) q s2 ->
+          step (st1 s1) E0 (st2 s1 s2)
+      | step_pop s1 r s2 s1':
+          Smallstep.final_state (L2 se) s2 r ->
+          Smallstep.after_external (L1 se) s1 r s1' ->
+          step (st2 s1 s2) E0 (st1 s1').
+
+      Inductive initial_state (q: query li): state -> Prop :=
+      | initial_state_intro s:
+          valid_query (L1 se) q = true ->
+          Smallstep.initial_state (L1 se) q s ->
+          initial_state q (st1 s).
+
+      Inductive at_external: state -> query li -> Prop :=
+      | at_external_intro s1 s2 q:
+          valid_query (L2 se) q = false ->
+          Smallstep.at_external (L2 se) s2 q ->
+          at_external (st2 s1 s2) q.
+
+      Inductive after_external: state -> reply li -> state -> Prop :=
+      | after_external_intro s1 s2 r s2':
+          Smallstep.after_external (L2 se) s2 r s2' ->
+          after_external (st2 s1 s2) r (st2 s1 s2').
+
+      Inductive final_state: state -> reply li -> Prop :=
+      | final_state_intro s r:
+          Smallstep.final_state (L1 se) s r ->
+          final_state (st1 s) r.
+
+    End WITH_SE.
+
+    Context (sk: AST.program unit unit).
+
+    Definition semantics: semantics li li :=
+      {|
+      activate se :=
+        {|
+          Smallstep.step ge := step se;
+          Smallstep.valid_query q := valid_query (L1 se) q || valid_query (L2 se) q;
+          Smallstep.initial_state := initial_state se;
+          Smallstep.at_external := at_external se;
+          Smallstep.after_external := after_external se;
+          Smallstep.final_state := final_state se;
+          Smallstep.globalenv := tt;
+          (* Genv.globalenv se p *)
+        |};
+      skel := sk;
+      |}.
+
+    Notation L := (fun i => match i with true => L1 | false => L2 end).
+    Hypothesis one_way_query: forall se q,
+        Smallstep.valid_query (L2 se) q = false ->
+        Smallstep.valid_query (L1 se) q = false.
+
+    Inductive state_match: state -> list (SmallstepLinking.frame L) -> Prop :=
+    | state_match1 s:
+        state_match (st1 s) (st L true s :: nil)
+    | state_match2 s1 s2:
+        state_match (st2 s1 s2) (st L false s2 :: st L true s1 :: nil).
+
+    Lemma catcomp_approx: forward_simulation 1 1 semantics (SmallstepLinking.semantics L sk).
+    Proof.
+      constructor. econstructor; eauto.
+      instantiate (1 := fun _ _ _ => _). cbn beta.
+      intros se ? [ ] [ ] Hsk.
+      apply forward_simulation_step with (match_states := state_match).
+      - intros q ? [ ]. reflexivity.
+      - intros q ? s1 [ ] Hq.
+        inv Hq. exists (st L true s :: nil).
+        split; constructor; auto.
+      - intros s1 s2 r Hs Hr.
+        inv Hr. inv Hs. exists r.
+        split; constructor; auto.
+      - intros s1 s2 q Hs Hq.
+        inv Hq. inv Hs.
+        exists tt. exists q. repeat apply conj; try constructor; auto.
+        + intros j. destruct j; auto.
+        + intros r1 ? s1' [ ] Hr.
+          inv Hr. exists (st L false s2' :: st L true s0 :: nil).
+          split; constructor; auto.
+      - intros sx t sx' Hstep sy Hs.
+        inv Hstep; inv Hs.
+        + exists (st L true s1' :: nil).
+          split; constructor; auto.
+        + exists (st L false s2' :: st L true s1 :: nil).
+          split; constructor; auto.
+        + exists (st L false s2 :: st L true s1 :: nil).
+          split; econstructor; eauto.
+        + exists (st L true s1' :: nil).
+          split; econstructor; eauto.
+      - apply well_founded_ltof.
+    Qed.
+  End CAT_COMP.
+End CAT_COMP.
+
+(* This is a special case of categorical composition, where the first component
+   is a closed transition system *)
+Section LAYER_COMP.
+  Context {li} (L1: semantics li li) (L2: semantics li_null li).
 
   Section WITH_SE.
     Context (se: Genv.symtbl).
@@ -36,14 +160,16 @@ Section CAT_COMP.
         Smallstep.initial_state (L1 se) q s ->
         initial_state q (st1 s).
 
-    Inductive at_external: state -> query li -> Prop :=
+    Inductive at_external: state -> query li_null -> Prop :=
     | at_external_intro s1 s2 q:
-        valid_query (L2 se) q = false ->
+        (* valid_query (L2 se) q = false -> *)
+        (* This should be empty *)
         Smallstep.at_external (L2 se) s2 q ->
         at_external (st2 s1 s2) q.
 
-    Inductive after_external: state -> reply li -> state -> Prop :=
+    Inductive after_external: state -> reply li_null -> state -> Prop :=
     | after_external_intro s1 s2 r s2':
+        (* This should be empty *)
         Smallstep.after_external (L2 se) s2 r s2' ->
         after_external (st2 s1 s2) r (st2 s1 s2').
 
@@ -56,7 +182,7 @@ Section CAT_COMP.
 
   Context (sk: AST.program unit unit).
 
-  Definition semantics: semantics li li :=
+  Definition layer_comp: semantics li_null li :=
     {|
     activate se :=
       {|
@@ -72,48 +198,191 @@ Section CAT_COMP.
     skel := sk;
     |}.
 
-  Notation L := (fun i => match i with true => L1 | false => L2 end).
-  Hypothesis one_way_query: forall se q,
-      Smallstep.valid_query (L2 se) q = false ->
-      Smallstep.valid_query (L1 se) q = false.
-
-  Inductive state_match: state -> list (SmallstepLinking.frame L) -> Prop :=
-  | state_match1 s:
-      state_match (st1 s) (st L true s :: nil)
-  | state_match2 s1 s2:
-      state_match (st2 s1 s2) (st L false s2 :: st L true s1 :: nil).
-
-  Lemma catcomp_approx: forward_simulation 1 1 semantics (SmallstepLinking.semantics L sk).
+  Lemma star_internal1 se s t s':
+    Star (L1 se) s t s' ->
+    star (fun _ => step se) tt (st1 s) t (st1 s').
   Proof.
-    constructor. econstructor; eauto.
-    instantiate (1 := fun _ _ _ => _). cbn beta.
-    intros se ? [ ] [ ] Hsk.
-    apply forward_simulation_step with (match_states := state_match).
-    - intros q ? [ ]. reflexivity.
-    - intros q ? s1 [ ] Hq.
-      inv Hq. exists (st L true s :: nil).
-      split; constructor; auto.
-    - intros s1 s2 r Hs Hr.
-      inv Hr. inv Hs. exists r.
-      split; constructor; auto.
-    - intros s1 s2 q Hs Hq.
-      inv Hq. inv Hs.
-      exists tt. exists q. repeat apply conj; try constructor; auto.
-      + intros j. destruct j; auto.
-      + intros r1 ? s1' [ ] Hr.
-        inv Hr. exists (st L false s2' :: st L true s0 :: nil).
-        split; constructor; auto.
-    - intros sx t sx' Hstep sy Hs.
-      inv Hstep; inv Hs.
-      + exists (st L true s1' :: nil).
-        split; constructor; auto.
-      + exists (st L false s2' :: st L true s1 :: nil).
-        split; constructor; auto.
-      + exists (st L false s2 :: st L true s1 :: nil).
-        split; econstructor; eauto.
-      + exists (st L true s1' :: nil).
-        split; econstructor; eauto.
-    - apply well_founded_ltof.
+    induction 1; [eapply star_refl | eapply star_step]; eauto.
+    constructor; auto.
   Qed.
-End CAT_COMP.
 
+  Lemma star_internal2 se s1 s t s':
+    Star (L2 se) s t s' ->
+    star (fun _ => step se) tt (st2 s1 s) t (st2 s1 s').
+  Proof.
+    induction 1; [eapply star_refl | eapply star_step]; eauto.
+    constructor; auto.
+  Qed.
+
+  Lemma plus_internal1 se s t s':
+    Plus (L1 se) s t s' ->
+    plus (fun _ => step se) tt (st1 s) t (st1 s').
+  Proof.
+    destruct 1; econstructor; eauto using step1, star_internal1.
+  Qed.
+
+  Lemma plus_internal2 se s1 s t s':
+    Plus (L2 se) s t s' ->
+    plus (fun _ => step se) tt (st2 s1 s) t (st2 s1 s').
+  Proof.
+    destruct 1; econstructor; eauto using step2, star_internal2.
+  Qed.
+
+(* TODO: prove that layer composition is equivalent to categorical composition
+  provided the bottom layer is closed *)
+End LAYER_COMP.
+
+Section LAYER_COMP_SIM.
+  Context {li cc} (L1 L1': semantics li li)
+          (HL1: fsim_components cc cc L1 L1').
+  Context (L2 L2': semantics li_null li)
+          (HL2: fsim_components 1 cc L2 L2').
+
+  Variant index :=
+  | index1: fsim_index HL1 -> index
+  | index2: fsim_index HL1 -> fsim_index HL2 -> index.
+
+  Variant order: index -> index -> Prop :=
+  | order1 i1 i1':
+      fsim_order HL1 i1 i1' ->
+      order (index1 i1) (index1 i1')
+  | order2 i1 i1' i2 i2' :
+      fsim_order HL2 i2 i2' ->
+      order (index2 i1 i2) (index2 i1' i2').
+
+  Context (se se': Genv.symtbl) (w : ccworld cc).
+  Context (Hse: match_senv cc w se se').
+  Context (Hse1: Genv.valid_for (skel L1) se).
+  Context (Hse2: Genv.valid_for (skel L2) se).
+
+  Variant match_states: index -> state L1 L2 -> state L1' L2' -> Prop :=
+  | match_states1 i s1 s1':
+      match_senv cc w se se' ->
+      fsim_match_states HL1 se se' w i s1 s1' ->
+      match_states (index1 i) (st1 L1 L2 s1) (st1 L1' L2' s1')
+  | match_states2 i i' w' s1 s1' s2 s2':
+      match_senv cc w se se' ->
+      match_senv cc w' se se' ->
+      fsim_match_states HL2 se se' w' i' s2 s2' ->
+      (forall r r' st1, match_reply cc w' r r' ->
+                   Smallstep.after_external (L1 se) s1 r st1 ->
+                   exists idx st1',
+                     Smallstep.after_external (L1' se') s1' r' st1' /\
+                     fsim_match_states HL1 se se' w idx st1 st1') ->
+      match_states (index2 i i') (st2 L1 L2 s1 s2) (st2 L1' L2' s1' s2').
+
+  Lemma step_fsim:
+    forall s1 t s2, step L1 L2 se s1 t s2 ->
+               forall i s1', match_states i s1 s1' ->
+                        exists i' s2', (plus (fun _ => step L1' L2' se') tt s1' t s2' \/
+                                   star (fun _ => step L1' L2' se') tt s1' t s2' /\ order i' i) /\
+                                  match_states i' s2 s2'.
+  Proof.
+    intros ? ? ? Hstep ? ? Hs.
+    destruct Hstep.
+    - inv Hs.
+      edestruct @fsim_simulation as (i' & s2' & Hstep' & Hs').
+      apply HL1. eauto. eauto. eauto. eauto.
+      eexists _, (st1 L1' L2' _). split.
+      + destruct Hstep'; [left | right].
+        apply plus_internal1. eauto.
+        split. now apply star_internal1. constructor. apply a.
+      + econstructor. eauto. eauto.
+    - inv Hs.
+      edestruct @fsim_simulation as (ix' & st2' & Hstep' & Hs').
+      apply HL2. eauto. eauto. eauto. eauto.
+      eexists _, (st2 L1' L2' _ _). split.
+      + destruct Hstep'; [left | right].
+        apply plus_internal2. eauto.
+        split. now apply star_internal2. constructor. apply a.
+      + econstructor. eauto. eauto. eauto. eauto.
+    - inv Hs.
+      edestruct @fsim_match_external as (w' & q' & Hext' & Hq' & Hse' & Haft).
+      apply HL1. eauto. eauto. eauto. eauto.
+      edestruct @fsim_match_initial_states as (? & ? & ? & ?).
+      apply HL2. eauto. eauto. eauto. eauto.
+      eexists _, (st2 L1' L2' _ _). split.
+      + left. apply plus_one. eapply step_push. eauto.
+        erewrite fsim_match_valid_query. eauto. apply HL2. eauto.
+        eauto. eauto. eauto.
+      + econstructor. eauto. eauto. eauto. eauto.
+    - inv Hs.
+      edestruct @fsim_match_final_states as (r' & Hf' & Hr).
+      apply HL2. eauto. eauto. eauto. eauto.
+      edestruct H8 as (? & ? & ? & ?). eauto. eauto.
+      eexists _, (st1 L1' L2' _). split.
+      + left. apply plus_one. eapply step_pop. eauto. eauto.
+      + econstructor. eauto. eauto.
+        Unshelve. auto. auto.
+  Qed.
+
+  Lemma external_fsim:
+    forall i s1 s2 q1,
+      match_states i s1 s2 ->
+      at_external L1 L2 se s1 q1 ->
+      exists (w0 : ccworld 1) (q2 : query li_null),
+        at_external L1' L2' se' s2 q2 /\
+        match_query 1 w0 q1 q2 /\
+        match_senv 1 w0 se se' /\
+        (forall r1 r2 s1',
+            match_reply 1 w0 r1 r2 ->
+            after_external L1 L2 se s1 r1 s1' ->
+            exists i' s2',
+              after_external L1' L2' se' s2 r2 s2' /\
+              match_states i' s1' s2').
+  Proof.
+    intros. destruct q1.
+  Qed.
+
+  Lemma initial_fsim:
+   forall q1 q2 s1,
+     match_query cc w q1 q2 ->
+     initial_state L1 L2 se q1 s1 ->
+     exists i s2,
+       initial_state L1' L2' se' q2 s2 /\
+       match_states i s1 s2.
+  Proof.
+    intros. inv H0.
+    edestruct @fsim_match_initial_states as (? & ? & ? & ?).
+    apply HL1. eauto. eauto. eauto. eauto.
+    eexists _, (st1 L1' L2' _). split.
+    - econstructor. erewrite @fsim_match_valid_query.
+      eauto. apply HL1. eauto. eauto. eauto. eauto.
+    - econstructor. eauto. eauto.
+  Qed.
+
+  Lemma final_fsim:
+    forall i s1 s2 r1,
+      match_states i s1 s2 ->
+      final_state L1 L2 se s1 r1 ->
+      exists r2,
+        final_state L1' L2' se' s2 r2 /\
+        match_reply cc w r1 r2.
+  Proof.
+    intros. inv H0. inv H.
+    edestruct @fsim_match_final_states.
+    apply HL1. eauto. eauto. eauto. eauto.
+    eexists. split. econstructor. apply H. apply H.
+  Qed.
+
+  Lemma layer_comp_fsim sk sk':
+    fsim_properties 1 cc se se' w
+                    (layer_comp L1 L2 sk se) (layer_comp L1' L2' sk' se')
+                    index order match_states.
+  Proof.
+    split. cbn in *.
+    - intros.
+      replace (valid_query (L1 se) q1) with (valid_query (L1' se') q2).
+      replace (valid_query (L2 se) q1) with (valid_query (L2' se') q2).
+      reflexivity.
+      erewrite @fsim_match_valid_query. eauto. apply HL2.
+      eauto. eauto. eauto.
+      erewrite @fsim_match_valid_query. eauto. apply HL1.
+      eauto. eauto. eauto.
+    - apply initial_fsim.
+    - apply final_fsim.
+    - apply external_fsim.
+    - apply step_fsim.
+  Qed.
+
+End LAYER_COMP_SIM.
--- compcertxo/Lifting.v
+++ compcertxo/Lifting.v
@@ -39,7 +39,6 @@ Section Lifting.
     |}%rel.
 End Lifting.
 
-
 Delimit Scope li_scope with li.
 Bind Scope li_scope with language_interface.
 Delimit Scope lts_scope with lts.
@@ -67,33 +66,37 @@ Definition no_perm_on (m: mem) (vars: block -> Z -> Prop): Prop :=
 
 Record krel {K1 K2: Type}: Type :=
   mk_krel {
-      Rk: K1 -> K2 -> Prop;
-      Rm: K1 -> mem -> Prop;
-      G: block -> Z -> Prop;
+      Rk: K1 -> K2 * mem -> Prop;    (* a simulation relation *)
+      G: block -> Z -> Prop;         (* private variables *)
 
+      Rkm '(k1, m1) '(k2, m2) :=
+        Rk k1 (k2, m2) /\ no_perm_on m1 G /\ Mem.extends m1 m2;
       G_unchanged:
-        forall k m1 m2,
-          Rm k m1 -> Mem.unchanged_on G m1 m2 -> Rm k m2;
+        forall k1 k2 m m',
+          Rk k1 (k2, m)  -> Mem.unchanged_on G m m' -> Rk k1 (k2, m');
     }.
 Arguments krel: clear implicits.
+
+Program Definition krel_comp {K1 K2 K3} (R1: krel K1 K2) (R2: krel K2 K3) :=
+  {|
+  Rk k1 '(k3, m3) := exists '(k2, m2), Rk R1 k1 (k2, m2) /\ Rk R2 k2 (k3, m3);
+  G b ofs := G R1 b ofs /\ G R2 b ofs;
+  |}.
+Next Obligation.
+Admitted.
 
 Section SIM_REL.
   Context {K1 K2} (R: krel K1 K2).
 
   Inductive kcc_ext_query: query (li_c @ K1) -> query (li_c @ K2) -> Prop :=
   | kcc_query_intro vf sg vargs1 vargs2 m1 m2 k1 k2:
-      Val.lessdef_list vargs1 vargs2 ->
-      Mem.extends m1 m2 ->
-      vf <> Vundef -> Rk R k1 k2 -> Rm R k1 m2 ->
-      no_perm_on m1 (G R) ->
+      Val.lessdef_list vargs1 vargs2 -> vf <> Vundef ->
+      Rkm R (k1, m1) (k2, m2) ->
       kcc_ext_query (cq vf sg vargs1 m1, k1) (cq vf sg vargs2 m2, k2).
 
   Inductive kcc_ext_reply: reply (li_c @ K1) -> reply (li_c @ K2) -> Prop :=
   | kcc_ext_reply_intro vres1 vres2 m1 m2 k1 k2:
-      Val.lessdef vres1 vres2 ->
-      Mem.extends m1 m2 ->
-      Rk R k1 k2 -> Rm R k1 m2 ->
-      no_perm_on m1 (G R) ->
+      Val.lessdef vres1 vres2 -> Rkm R (k1, m1) (k2, m2) ->
       kcc_ext_reply (cr vres1 m1, k1) (cr vres2 m2, k2).
 
   Program Definition kcc_ext :=
@@ -106,67 +109,113 @@ Section SIM_REL.
 
 End SIM_REL.
 
-Module Ksim.
-  Record ksim {K1 K2: Type} :=
-    mk_ksim {
-        L1: Smallstep.semantics li_null (li_c @ K1);
-        L2: Smallstep.semantics li_null (li_c @ K2);
-        R: krel K1 K2;
-
-        layer_sim: forward_simulation 1 (kcc_ext R) L1 L2;
-      }.
-  Arguments ksim: clear implicits.
-End Ksim.
-
-Definition lts_closed {liA liB} (L: semantics liA liB) :=
-  forall se s q, ~ (at_external (L se)) s q.
-
-Lemma li_null_closed {li} (L: semantics li_null li): lts_closed L.
-Proof.
-  unfold lts_closed.
-  intros. inversion q.
-Qed.
-
-Section LTS_CLOSED.
-
-  Context {liA liB} (L: semantics liA liB).
-  Context (HL: lts_closed L).
-
-  Variable liX: language_interface.
-  Definition lts' se: lts liX liB (state L) :=
-    {|
-    genvtype := genvtype (L se);
-    step ge := step (L se) ge;
-    valid_query := valid_query (L se);
-    initial_state := initial_state (L se);
-    at_external := fun s q => False;
-    after_external := fun q s r => False;
-    final_state := final_state (L se);
-    globalenv := globalenv (L se)
-    |}.
-  Definition closed_sem_conv :=
-    {|
-    skel := skel L;
-    activate se := lts' se;
-    |}.
-
-  (* TODO: proved equivalence *)
-End LTS_CLOSED.
-
 Require Import Clight.
+
+Inductive state_match {K1 K2} (R: krel K1 K2): rel (state * K1) (state * K2) :=
+| State_match f s k e te m1 m2 k1 k2:
+    Rkm R (k1, m1) (k2, m2) ->
+    state_match R (State f s k e te m1, k1) (State f s k e te m2, k2)
+| Callstate_match v vs k m1 m2 k1 k2:
+    (* less_def on arguments? *)
+    Rkm R (k1, m1) (k2, m2) ->
+    state_match R (Callstate v vs k m1, k1) (Callstate v vs k m2, k2)
+| Returnstate_match v k m1 m2 k1 k2:
+    Rkm R (k1, m1) (k2, m2) ->
+    state_match R (Returnstate v k m1, k1) (Returnstate v k m2, k2).
+
+Lemma clight_lifted {K1 K2} p R:
+  forward_simulation (kcc_ext R) (kcc_ext R)
+                     ((semantics1 p) @ K1) ((semantics1 p) @ K2).
+Proof.
+  constructor. econstructor; eauto. instantiate (1  := fun _ _ _ => _). cbn beta.
+  intros se ? [ ] [ ] Hsk.
+  apply forward_simulation_step with (match_states := state_match R).
+  - intros. simpl. admit.
+  -
+
+Admitted.
+
+Notation " 'layer' K " := (Smallstep.semantics li_null (li_c @ K)) (at level 1).
+
+Definition layer_sim {K1 K2: Type} (L1: layer K1) (L2: layer K2)
+           (R: krel K1 K2) := forward_simulation 1 (kcc_ext R) L1 L2.
+
+(* Section Layer_Lifting. *)
+(*   Variable K: Type. *)
+(*   Context {li state} (L: lts li_null li state). *)
+
+(*   Definition lifted_lts_layer: lts li_null (li @ K) (state * K) := *)
+(*     {| *)
+(*     genvtype := genvtype L; *)
+(*     step ge := step' K L ge; *)
+(*     valid_query := fun (q: query (li @ K)) => valid_query L (fst q); *)
+(*     initial_state := (initial_state L) * eq; *)
+(*     at_external '(s, _) := at_external L s; *)
+(*     after_external '(s, _) r '(s', _) := after_external L s r s'; *)
+(*     final_state := (final_state L) * eq; *)
+(*     globalenv := globalenv L *)
+(*     |}%rel. *)
+(* End Layer_Lifting. *)
+
+(* Definition lifted_semantics_layer {li} (K: Type) (L: semantics li_null li) := *)
+(*   {| *)
+(*   skel := skel L; *)
+(*   activate se := lifted_lts_layer K (L se); *)
+(*   |}. *)
+
+(* Notation "L @ K" := (lifted_semantics_layer K L): sem_scope. *)
+
+
+
+(* TODO: move this to somewhere else *)
+(* Definition lts_closed {liA liB} (L: Smallstep.semantics liA liB) := *)
+(*   forall se s q, ~ (Smallstep.at_external (L se)) s q. *)
+
+(* Lemma li_null_closed {li} (L: semantics li_null li): lts_closed L. *)
+(* Proof. *)
+(*   unfold lts_closed. *)
+(*   intros. inversion q. *)
+(* Qed. *)
+
+(* Section LTS_CLOSED. *)
+
+(*   Context {liA liB} (L: semantics liA liB). *)
+(*   Context (HL: lts_closed L). *)
+
+(*   Variable liX: language_interface. *)
+(*   Definition lts' se: lts liX liB (state L) := *)
+(*     {| *)
+(*     genvtype := genvtype (L se); *)
+(*     step ge := step (L se) ge; *)
+(*     valid_query := valid_query (L se); *)
+(*     initial_state := initial_state (L se); *)
+(*     at_external := fun s q => False; *)
+(*     after_external := fun q s r => False; *)
+(*     final_state := final_state (L se); *)
+(*     globalenv := globalenv (L se) *)
+(*     |}. *)
+(*   Definition closed_sem_conv := *)
+(*     {| *)
+(*     skel := skel L; *)
+(*     activate se := lts' se; *)
+(*     |}. *)
+
+(*   (* TODO: proved equivalence *) *)
+(* End LTS_CLOSED. *)
+
+
 Require Import CatComp.
 Require Import Linking.
 
-Definition layer_combine {K} (M: Clight.program)
-           (L: Smallstep.semantics li_null (li_c @ K)) sk :=
-  let L' := CatComp.semantics (semantics1 M @ K) (closed_sem_conv L (li_c @ K)) sk
-  in closed_sem_conv L' li_null.
 
-
+Definition layer_combine {K} (M: Clight.program) (L: layer K) sk :=
+  (* let L' := CatComp.semantics (semantics1 M @ K) (closed_sem_conv L (li_c @ K)) sk *)
+  (* in closed_sem_conv L' li_null. *)
+  layer_comp (semantics1 M @ K) L sk.
 Record prog_ksim {K1 K2: Type} :=
   mk_prog_ksim {
-      L1: Smallstep.semantics li_null (li_c @ K1);
-      L2: Smallstep.semantics li_null (li_c @ K2);
+      L1: layer K1;
+      L2: layer K2;
       M: Clight.program;
       R: krel K1 K2;
       sk: AST.program unit unit;
@@ -186,5 +235,22 @@ Section PROG_SIM.
   Qed.
 End PROG_SIM.
 Coercion prog_sim_coercion : prog_ksim >-> Ksim.ksim.
+
+Section VCOMP.
+
+  Context {K1 K2 K3 L1 L2 L3} {M N: Clight.program}
+          {R: krel K1 K2} {S: krel K2 K3} {sk1 sk2}
+          (Hsk1: link (skel L2) (skel (semantics1 M)) = Some sk1)
+          (Hsk2: link (skel L3) (skel (semantics1 N)) = Some sk2)
+          (HL1: forward_simulation 1 (kcc_ext R) L1 (layer_combine M L2 sk1))
+          (HL2: forward_simulation 1 (kcc_ext S) L2 (layer_combine N L3 sk2)).
+  Context (p: Clight.program) (Hlk: link M N = Some p).
+
+  Theorem layer_vcomp:
+    forward_simulation 1 (kcc_ext (krel_comp R S)) L1 (layer_combine p L3 sk2).
+  Proof.
+
+End VCOMP.
+
 
 Require Import FlatComp.
