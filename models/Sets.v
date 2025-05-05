aecfdb030bcbf060b2e044a3a4c6d9308601745a
Jeremie Koenig <jeremie.koenig@yale.edu>
2025-05-05 17:19:43 -0400

    SET: add cartesian closed structure

--- models/Sets.v
+++ models/Sets.v
@@ -1,4 +1,5 @@
 Require Import interfaces.Category.
+Require Import interfaces.Functor.
 Require Import interfaces.FunctorCategory.
 Require Import interfaces.MonoidalCategory.
 Require Import FunctionalExtensionality.
@@ -6,7 +7,9 @@ Require Import FunctionalExtensionality.
 
 (** * Category of sets and functions *)
 
-Module SET <: CategoryWithEndofunctors.
+Module SetBase <: CategoryWithEndofunctors.
+
+  (** ** Core definitions *)
 
   Definition t := Type.
   Definition m X Y := X -> Y.
@@ -33,6 +36,9 @@ Module SET <: CategoryWithEndofunctors.
   Qed.
 
   Include CategoryTheory.
+
+  (** ** Endofunctors *)
+
   Include AddEndofunctors.
 
   (** Naturality properties in [SET] are more convenient to use in the
@@ -47,7 +53,129 @@ Module SET <: CategoryWithEndofunctors.
     rewrite End.natural. reflexivity.
   Qed.
 
-End SET.
+End SetBase.
+
+Module Type SetBaseSpec.
+  Include SetBase.
+End SetBaseSpec.
+
+(** ** Monoidal Structures *)
+
+Module SetMonoidalStructures (B : SetBaseSpec).
+  Import B.
+
+  Module Prod <: CartesianStructure B.
+
+    Definition unit := Datatypes.unit.
+    Definition ter X : X ~~> unit := fun _ => tt.
+
+    Theorem ter_uni {X} :
+      forall (x y : X ~~> unit), x = y.
+    Proof.
+      intros.
+      apply functional_extensionality.
+      intros i. destruct (x i), (y i); auto.
+    Qed.
+
+    Definition omap : t -> t -> t := prod.
+    Definition p1 {A B} : omap A B ~~> A := @fst A B.
+    Definition p2 {A B} : omap A B ~~> B := @snd A B.
+    Definition pair {X A B} (f : X ~~> A) (g : X ~~> B) : X ~~> omap A B :=
+      fun x => (f x, g x).
+
+    Theorem p1_pair {X A B} (f : m X A) (g : m X B) :
+      p1 @ pair f g = f.
+    Proof.
+      reflexivity.
+    Qed.
+
+    Theorem p2_pair {X A B} (f : m X A) (g : m X B) :
+      p2 @ pair f g = g.
+    Proof.
+      firstorder.
+    Qed.
+
+    Theorem pair_pi {X A B} x :
+      @pair X A B (p1 @ x) (p2 @ x) = x.
+    Proof.
+      apply functional_extensionality.
+      intros i. unfold pair, compose.
+      destruct x; auto.
+    Qed.
+
+    Include CartesianStructureTheory B.
+    Include BifunctorTheory B B B.
+  End Prod.
+
+  Module Exp : MonoidalClosureDefinition B Prod.
+
+    Definition omap (A B : t) : t :=
+      A -> B.
+
+    Definition fmap {A1 A2 B1 B2} (f : B1 ~~> A1) (g : A2 ~~> B2) : omap A1 A2 ~~> omap B1 B2 :=
+      fun h => g @ h @ f.
+
+    Theorem fmap_id A1 A2 :
+      fmap (id A1) (id A2) = id (omap A1 A2).
+    Proof.
+      reflexivity.
+    Qed.
+
+    Theorem fmap_compose {A1 A2 B1 B2 C1 C2} :
+      forall (g1 : C1 ~~> B1) (g2 : B2 ~~> C2) (f1 : B1 ~~> A1) (f2 : A2 ~~> B2),
+        fmap (f1 @ g1) (g2 @ f2) = fmap g1 g2 @ fmap f1 f2.
+    Proof.
+      reflexivity.
+    Qed.
+
+    Include BifunctorTheory B.Op B B.
+
+    Definition curry : forall {A B C}, (Prod.omap A B ~~> C) -> (A ~~> omap B C) :=
+      @Datatypes.curry.
+
+    Definition uncurry : forall {A B C}, (A ~~> omap B C) -> (Prod.omap A B ~~> C) :=
+      @Datatypes.uncurry.
+
+    Theorem uncurry_curry {A B C} (f : Prod.omap A B ~~> C) :
+      uncurry (curry f) = f.
+    Proof.
+      apply functional_extensionality.
+      intros [a b].
+      reflexivity.
+    Qed.
+
+    Theorem curry_uncurry {A B C} (g : A ~~> omap B C) :
+      curry (uncurry g) = g.
+    Proof.
+      reflexivity.
+    Qed.
+
+    Theorem curry_natural_l {A1 A2 B C} (x : A1 ~~> A2) (f : Prod.omap A2 B ~~> C) :
+      curry (f @ (Prod.fmap x (id B))) = curry f @ x.
+    Proof.
+      reflexivity.
+    Qed.
+
+    Theorem curry_natural_r {A B C1 C2} (f : Prod.omap A B ~~> C1) (y : C1 ~~> C2) :
+      curry (y @ f) = fmap (id B) y @ curry f.
+    Proof.
+      reflexivity.
+    Qed.
+
+    Include MonoidalClosureTheory B Prod.
+  End Exp.
+End SetMonoidalStructures.
+
+(** ** Overall definition *)
+
+Module Type SetSpec :=
+  CartesianCategory <+
+  AddEndofunctors.
+
+Module SET <: SetSpec :=
+  SetBase <+
+  SetMonoidalStructures.
+
 
 (** ** The terms for an effect signature [E] define a monad *)
 
