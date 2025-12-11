a54bd60de01252441c8a5f91f906246f6a971833
Arthur Vale <34468091+arthurovale@users.noreply.github.com>
2025-12-11 17:37:26 -0500

    Create DoubleKaroubi.v

---
+++ interfaces/DoubleKaroubi.v
@@ -0,0 +1,27 @@
+Require Import interfaces.Category.
+Require Import interfaces.Functor.
+Require Import interfaces.DoubleCat.
+
+Module Idempotents (V : CategoryDefinition) (D : DoubleCategory V).
+  Import D.
+  Import Siso.
+
+  Record idem (a : V.t) :=
+    {
+      carrier :> hcell a a;
+      idempotency : sisocell (carrier ⨀ carrier) carrier;
+      idem_coh : (idempotency ⊙ vid carrier) ;; idempotency =
+        assoc carrier carrier carrier ;; (vid carrier ⊙ idempotency) ;; idempotency;
+    }.
+
+  Program Definition id_idem (a : V.t) :=
+    {|
+      carrier := hid a;
+      idempotency := runit (hid a);
+    |}.
+  Next Obligation.
+    rewrite <- lunit_hid_runit_hid. rewrite <- unit_coh.
+    rewrite lunit_hid_runit_hid. reflexivity.
+  Qed.
+
+End Idempotents.
